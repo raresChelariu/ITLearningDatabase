1d2b93ef08e90d526b124b9e9e000e0cd671880c
Rares Chelariu <Rares.Chelariu@nuvei.com>
2023-03-11 13:51:55 +0200

    adding videos table

---
+++ dbo/Tables/Videos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Videos] (
+    [RowID]       BIGINT          CONSTRAINT [DF_Videos] DEFAULT (NEXT VALUE FOR [SEQ_Videos]) NOT NULL,
+    [FileContent] VARBINARY (MAX) NOT NULL,
+    [Filename]    VARCHAR (MAX)   NOT NULL,
+    CONSTRAINT [PK_Videos_RowID] PRIMARY KEY CLUSTERED ([RowID] ASC)
+);
+
